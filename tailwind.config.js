f78e09183b71b6ddc1f406bc1fa7d3bdc5932575
aoshien <aoshisen@fanxiapp.com>
2022-06-07 17:06:17 +0800

    some mofify

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,6 @@ module.exports = {
         "./src/**/*.{html,js,ts,jsx,tsx,pug}",
     ],
     theme: {
-        minHeight: {
-            '0': '0',
-            '1/4': '25%',
-            '1/2': '50%',
-            '3/4': '75%',
-            'content': "80vh",
-            'full': '100%',
-        },
         extend: {},
     },
     plugins: [],
