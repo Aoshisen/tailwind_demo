cad8b2796c80ae2197a6bf409059705bbef33bbd
aoshien <aoshisen@fanxiapp.com>
2022-06-15 09:39:06 +0800

    修复less 工作区使用tailwind语法报错

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,7 @@
 module.exports = {
-    content: [
-        "./src/**/*.{html,js,ts,jsx,tsx,pug}",
-    ],
-    theme: {
-        extend: {},
-    },
     plugins: [
         require('postcss-import'),
         require('tailwindcss'),
         require('autoprefixer'),
-    ],
+    ]
 }
--- src/scripts/banner.js
+++ src/scripts/banner.js
@@ -1,15 +1,12 @@
-console.log('this is index.js');
+//banner 切换的js
 let bannerImgEl = $('.banner img')
-
 let timer = 0;
-let total = 2;
+let total = bannerImgEl.length;
 setInterval(() => {
     //当前循环应该渲染的图片
     let current = timer % total;
-
     bannerImgEl.filter((index) => {
         if (index == current) {
-            console.log('hidden');
             bannerImgEl.eq(index).css('display', "none")
         } else {
             bannerImgEl.eq(index).css('display', "inline-block")
